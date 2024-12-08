f556d33fa9cfa6eb742a25c102620dd172e68f9b
Mechanic Josh <joshuadunbar@gmail.com>
2024-12-08 13:27:21 -0500

    complete functional App, needs styling

--- src/client/views/CreatePost.tsx
+++ src/client/views/CreatePost.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { fetchData } from '../services/fetchData'; 
 
 interface CreatePostProps {}
 
 const CreatePost = (props: CreatePostProps) => {
+    const navigate = useNavigate();
+    const [value, setValue] = useState<string>(''); 
+
+    const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        fetchData('/api/chirps', 'POST', { body: value})
+        .then(data => navigate(`/chirps/${data.id}`));
+    }
+
     return(
         <main className="container mt-5">
             <section className="row justify-content-center">
-                <h1 className="text-center text-primary">Create Post Page</h1>
+                <div className="col-12 col-md-6">
+                    <form className="p-4 shadow border">
+                        <label htmlFor="body">What Are You Thinking About?</label>
+                        <input 
+                            value={value}
+                            onChange={e => setValue(e.target.value)}
+                            type="text" 
+                            className='form-control' 
+                            placeholder='Write Your Post Here'
+                        />
+                        <button onClick={handleSubmit} className="mt-3 btn btn-primary">Add Chirp</button>
+                    </form>
+                </div>
             </section>
         </main>
     )
